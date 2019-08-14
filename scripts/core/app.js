e528733eb18a4596306e0d8dd39090016b4bf934
Jasper De Jesus <jaspercdejesus@gmail.com>
2019-08-14 13:58:30 +0800

    tilesheet test

--- scripts/core/app.js
+++ scripts/core/app.js
@@ -1,26 +1,40 @@
-let type = "WebGL";
-if (!PIXI.utils.isWebGLSupported()){
-    type = "canvas"
-}
-PIXI.utils.sayHello(type);
+let Application = PIXI.Application,
+    Rectangle = PIXI.Rectangle,
+    loader = PIXI.Loader.shared,
+    TextureCache = PIXI.utils.TextureCache,
+    resources = PIXI.Loader.shared.resources,
+    Sprite = PIXI.Sprite;
 
-
-let app = new PIXI.Application({
+let app = new Application({
     width: 800,
     height: 600,
     transparent: false,
     antialias: true
 });
-
-let texture = PIXI.utils.TextureCache["images/dices.png"];
-let sprite = new PIXI.Sprite(texture);
-let loader = new PIXI.Loader();
-PIXI.loader.add("images/dices.png").load(setup);
-
-function setup() {
-    let sprite = new PIXI.Sprite(
-        PIXI.loader.resources["images/dices.png"].texture
-    );
-    app.stage.addChild(sprite);
-}
 document.body.appendChild(app.view);
+
+loader
+    .add("images/tileset.png")
+    .on("progress", onLoaderProgress)
+    .load(setup);
+function onLoaderProgress(loader, resource) {
+    console.log("loading: " + resource.url);
+    console.log("progress:" + loader.progress + "%");
+}
+
+function setup(){
+    let texture = TextureCache["images/tileset.png"];
+
+    let rectangle = new Rectangle(128+64, 128, 64, 64);
+
+    texture.frame = rectangle;
+
+    let rocket = new Sprite(texture);
+
+    rocket.x = 32;
+    rocket.y = 32;
+
+    app.stage.addChild(rocket);
+
+    app.renderer.render(app.stage);
+}
