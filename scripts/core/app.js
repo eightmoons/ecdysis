0b7d89f236910be155e602ffd4de6b6e0a69cdf7
Jasper de Jesus <jasperdj00@gmail.com>
2019-08-18 18:30:03 +0800

    [FIX] sprite naming

--- scripts/core/app.js
+++ scripts/core/app.js
@@ -1,26 +1,59 @@
-let type = "WebGL";
-if (!PIXI.utils.isWebGLSupported()){
-    type = "canvas"
-}
-PIXI.utils.sayHello(type);
+let Application = PIXI.Application,
+    Container = PIXI.Container,
+    loader = PIXI.Loader.shared,
+    resources = PIXI.Loader.shared.resources,
+    TextureCache = PIXI.utils.TextureCache,
+    Sprite = PIXI.Sprite,
+    Text = PIXI.Text,
+    TextStyle = PIXI.TextStyle
+;
 
-
-let app = new PIXI.Application({
+let app = new Application({
     width: 800,
     height: 600,
     transparent: false,
     antialias: true
 });
-
-let texture = PIXI.utils.TextureCache["images/dices.png"];
-let sprite = new PIXI.Sprite(texture);
-let loader = new PIXI.Loader();
-PIXI.loader.add("images/dices.png").load(setup);
-
-function setup() {
-    let sprite = new PIXI.Sprite(
-        PIXI.loader.resources["images/dices.png"].texture
-    );
-    app.stage.addChild(sprite);
-}
+app.renderer.backgroundColor = 0xECECEC;
 document.body.appendChild(app.view);
+
+loader
+    .add("images/ecdysis.json")
+    .on("progress", onLoaderProgress)
+    .load(main);
+
+function onLoaderProgress(loader, resource) {
+    console.log("loading: " + resource.url);
+    console.log("progress:" + loader.progress + "%");
+}
+//scenes
+let mainMenuScene, inGameScene;
+
+//in-game objects
+function main(){
+    inGameScene = new Container();
+    id = resources["images/ecdysis.json"].textures;
+    sheet = resources["images/ecdysis.json"].spritesheet;
+    let slimeFrames = ["slime1-1.png","slime1-2.png","slime1-3.png","slime1-4.png","slime1-5.png"];
+    let slimeTexture = [];
+    for (let i = 0; i < 5; i++) {
+        slimeTexture.push(sheet.textures[slimeFrames[i]]);
+    }
+    let slime = new PIXI.AnimatedSprite(slimeTexture);
+    slime.x = 150;
+    slime.y = 100;
+    slime.anchor.set(0.5,0.5);
+    slime.animationSpeed =0.15;
+    slime.play();
+
+    app.stage.addChild(slime);
+
+    state = play;
+    app.ticker.add(delta => gameLoop(delta));
+}
+
+function gameLoop(delta) {
+}
+
+function play(delta){
+}
