bb19431ea44f476b670dc32e1bfeda27284690d9
Jasper de Jesus <jasperdj00@gmail.com>
2019-08-18 14:17:27 +0800

    dungeon hunter fix

--- scripts/core/app.js
+++ scripts/core/app.js
@@ -17,7 +17,6 @@ let app = new Application({
 let stage = app.stage;
 document.body.appendChild(app.view);
 
-
 loader
     .add("images/dungeonhunter.json")
     .on("progress", onLoaderProgress)
@@ -279,5 +278,49 @@ function setMovementManager(sprite, base_speed) {
             sprite.vy = 0;
         }
     };
+}
 
+function keyboard(value) {
+    let key = {};
+    key.value = value;
+    key.isDown = false;
+    key.isUp = true;
+    key.press = undefined;
+    key.release = undefined;
+
+    key.downHandler = event => {
+        if (event.key === key.value) {
+            if (key.isUp && key.press) key.press();
+            key.isDown = true;
+            key.isUp = false;
+            event.preventDefault();
+        }
+    };
+
+    key.upHandler = event => {
+        if (event.key === key.value) {
+            if (key.isDown && key.release) key.release();
+            key.isDown = false;
+            key.isUp = true;
+            event.preventDefault();
+        }
+    };
+
+    const downListener = key.downHandler.bind(key);
+    const upListener = key.upHandler.bind(key);
+
+    window.addEventListener(
+        "keydown", downListener, false
+    );
+
+    window.addEventListener(
+        "keyup", upListener, false
+    );
+
+    key.unsubscribe = ( ) => {
+        window.removeEventListener("keydown", downListener);
+        window.removeEventListener("keyup", upListener);
+    }
+
+    return key;
 }
